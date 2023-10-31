6aa3e22385282507faf59bfb8828f38ba9665614
LDS196 <lihachevd87@gmail.com>
2023-10-31 21:06:53 +0400

    refactor:

--- src/Resume/Experience.tsx
+++ src/Resume/Experience.tsx
@@ -4,43 +4,39 @@ import s from "./Experience.module.scss";
 export const Experience = () => {
     return (
         <div className={s.experience}>
-            <ul className={s.list}>
-                <li className={s.place}>Frontend Developer</li>
-                <li className={s.year}>April 2023 - present day </li>
-                <li className={s.speciality}><span>Panels</span><br/>
-                      Project: development of an administrative panel to optimize the provision of SMM services in a team of 10 people.<br/>
-                    Completed tasks:<br/>
-                    • developed the project architecture<br/>
-                    • created adaptive components<br/>
-                    • configured Eslint and Prettier<br/>
-                    • configured VueRouter and Vuex<br/>
-                     Core stack: Vue.js, Vuex, TypeScript<br/>
-                     Link: panel.makeyourpanel.com/admin<br/>
-                </li>
-              <li className={s.speciality}><span></span><br/>
-                Project: development, support and updating of the functionality of a web application for a corporate social network in a team of 5 people.<br/>
-                Completed tasks:<br/>
-                • took part in the development of the project structure<br/>
-                • developed reusable adaptive React components<br/>
-                • organized Redux storage<br/>
-                • interacted with the server and processed errors<br/>
-                • tested React components using Jest<br/>
-                • was involved in the creation of modal windows and user authorization<br/>
-                Core stack: NextJS, TypeScript, RTK Query.<br/>
-
-              </li>
-
-
-            </ul>
-            <ul className={s.list}>
-                <li className={s.place}>Engineer Railway Transport</li>
-                <li className={s.year}>OAO RZD (Yekaterinburg 2010-2022)</li>
-                <li className={s.speciality}>Lead a construction team service of railway supervise the work of
-                    construction equipment preparation of reports on work planning and control of completed work
-                    financial responsibility, financial reporting development of technical documentation
-                </li>
-
-            </ul>
+            {/*<ul className={s.list}>*/}
+            {/*    <li className={s.place}>Frontend Developer</li>*/}
+            {/*    <li className={s.year}>April 2023 - present day </li>*/}
+            {/*    <li className={s.speciality}><span>Panels</span><br/>*/}
+            {/*          Project: development of an administrative panel to optimize the provision of SMM services in a team of 10 people.<br/>*/}
+            {/*        Completed tasks:<br/>*/}
+            {/*        • developed the project architecture<br/>*/}
+            {/*        • created adaptive components<br/>*/}
+            {/*        • configured Eslint and Prettier<br/>*/}
+            {/*        • configured VueRouter and Vuex<br/>*/}
+            {/*         Core stack: Vue.js, Vuex, TypeScript<br/>*/}
+            {/*         Link: panel.makeyourpanel.com/admin<br/>*/}
+            {/*    </li>*/}
+            {/*  <li className={s.speciality}><span></span><br/>*/}
+            {/*    Project: development, support and updating of the functionality of a web application for a corporate social network in a team of 5 people.<br/>*/}
+            {/*    Completed tasks:<br/>*/}
+            {/*    • took part in the development of the project structure<br/>*/}
+            {/*    • developed reusable adaptive React components<br/>*/}
+            {/*    • organized Redux storage<br/>*/}
+            {/*    • interacted with the server and processed errors<br/>*/}
+            {/*    • tested React components using Jest<br/>*/}
+            {/*    • was involved in the creation of modal windows and user authorization<br/>*/}
+            {/*    Core stack: NextJS, TypeScript, RTK Query.<br/>*/}
+            {/*  </li>*/}
+            {/*</ul>*/}
+            {/*<ul className={s.list}>*/}
+            {/*    <li className={s.place}>Engineer Railway Transport</li>*/}
+            {/*    <li className={s.year}>OAO RZD (Yekaterinburg 2010-2022)</li>*/}
+            {/*    <li className={s.speciality}>Lead a construction team service of railway supervise the work of*/}
+            {/*        construction equipment preparation of reports on work planning and control of completed work*/}
+            {/*        financial responsibility, financial reporting development of technical documentation*/}
+            {/*    </li>*/}
+            {/*</ul>*/}
         </div>
 
     );
