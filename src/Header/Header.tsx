a95920d24727d67f43bf901443a1f61c316e0de4
LDS196 <lihachevd87@gmail.com>
2024-03-22 15:50:52 +0400

    feat: add some animations with Framer-motion lib

--- src/Header/Header.tsx
+++ src/Header/Header.tsx
@@ -2,39 +2,46 @@ import React from 'react';
 import s from './Header.module.scss'
 import logo from '../assets/images/lab.png'
 import {Link} from "react-scroll";
-
-
-
+import {motion} from 'framer-motion';
 
 
 const Header = () => {
-    return (
-        <div className={s.header} >
-            <Link to={'/'}>
-                <div className={s.headerLogo}>
-                    <img src={logo} alt="logo" className={s.logoImg}/>
-                    <p className={s.logoText}>LDS LAB</p>
-                </div>
-            </Link>
-
-            <nav className={s.nav}>
-                <ul className={s.items}>
-                    <Link className={s.link} to={'intro'}>
-                        <li className={s.item}>home</li>
-                    </Link>
-                    <Link className={s.link} to={'projects'}>
-                        <li className={s.item}>my projects</li>
-                    </Link>
-                    <Link className={s.link} to={'resume'}>
-                        <li className={s.item}>resume</li>
-                    </Link>
-                    <Link className={s.link} to={'contacts'}>
-                        <li className={s.item}>contacts</li>
-                    </Link>
-                </ul>
-            </nav>
+  return (
+    <div className={s.header}>
+      <Link to={'/'}>
+        <div className={s.headerLogo}>
+          <motion.img src={logo} alt="logo" className={s.logoImg} animate={{
+            rotate: 180
+          }}
+                      transition={{duration: 1, repeat: Infinity, repeatDelay: 2}}/>
+          <p className={s.logoText}>LDS LAB
+          </p>
         </div>
-    );
+      </Link>
+
+      <nav className={s.nav}>
+        <ul className={s.items}>
+
+          <Link className={s.link} to={'intro'}>
+            <li className={s.item}>home</li>
+          </Link>
+
+
+          <Link className={s.link} to={'projects'}>
+            <li className={s.item}>my projects</li>
+          </Link>
+          <Link className={s.link} to={'resume'}>
+            <li className={s.item}>resume</li>
+          </Link>
+          <Link className={s.link} to={'contacts'}>
+            <li className={s.item}>contacts</li>
+          </Link>
+        </ul>
+      </nav>
+    </div>
+  )
+    ;
 };
+
 
 export default Header;
--- src/Intro/Intro.tsx
+++ src/Intro/Intro.tsx
@@ -12,48 +12,83 @@ import js from '../assets/images/skills-logo/js.png'
 import materialUI from '../assets/images/skills-logo/material-ui.svg'
 import sass from '../assets/images/skills-logo/sass.png'
 import storybook from '../assets/images/skills-logo/storybook.svg'
-import ReactTypingEffect from "react-typing-effect";
+import {motion} from 'framer-motion';
 
 
 const mySkills = [react, js, redux, css, figma, git, html5, materialUI, sass, storybook,]
+
+
 const Intro = () => {
-    return (
-        <section className={s.intro} id={'intro'}>
-            <div className={s.intro__block}>
-                <div className={s.about}>
-                    <p className={s.about__subtitle}>welcome to my world</p>
-                    <h1 className={s.about__title}>Hi, I'm <span className={s.about__name}>Dmitrii </span>
-                        <br/>
-                      a Frontend Developer
-                        {/*<ReactTypingEffect*/}
-                        {/*  className={s.typing}*/}
-                        {/*  text={'a Frontend Developer'}*/}
+  const title = 'a Frontend Developer'
 
-                        {/*                   speed={150}*/}
-                        {/*                   typingDelay={500}*/}
-                        {/*                   eraseSpeed={45}*/}
-                        {/*                   eraseDelay={3000}*/}
-                        {/*/>*/}
-                    </h1>
-                    <p className={s.about__desc}>Frontend developer with two years of development experience. I write in Javascript/TypeScript using React, Next.js, Vue. I love developing SPA/SSR web applications with user-friendly UI and beautiful design.<br/>I am passionate about information technology and how it is transforming business.
-                        <br/>I will be glad to work in a group with current technology stacks and high-level expertise.</p>
-                    <p className={s.about__skill}>best skill on</p>
-                    <ul className={s.about__items}>
-                        {mySkills.map((el, i) => <li key={i}><img className={s.about__item} src={el} alt=""/></li>)}
-                    </ul>
-                </div>
-                <div className={s.avatar}>
 
-                        <div className={s.avatar__block}>
-                            <img src={avatar} alt="avatar"/>
-                        </div>
+  return (
+    <section className={s.intro} id={'intro'}>
+      <div className={s.intro__block}>
+        <div className={s.about}>
+          <p className={s.about__subtitle}>welcome to my world</p>
 
-                </div>
+          <h1 className={s.about__title}>Hi, I'm <span className={s.about__name}>Dmitrii </span>
+            <br/>
+            {[...title].map((letter, index) => (
+              <motion.span
+                key={index}
+                style={{
+                  minWidth: '15px',
+                  marginTop: '10px',
+                  display: 'inline-block',
+                  originY: 0.7, // Adjust this value to control the jump height
+                }}
+                animate={{
+                  y: [-20, 10, -20], // Define the jump motion
+                }}
+                transition={{
+                  duration: 0.6,
+                  repeat: Infinity,
+                  repeatDelay: 2,
+                  delay: index * 0.1, // Add delay for staggered animation
+                }}
+              >
+                {letter}
+              </motion.span>
+            ))}
+          </h1>
 
-            </div>
-            <Line/>
-        </section>
-    );
+
+          <p className={s.about__desc}>Frontend developer with two years of development experience. I write in
+            Javascript/TypeScript using React, Next.js, Vue. I love developing SPA/SSR web applications with
+            user-friendly UI and beautiful design.<br/>I am passionate about information technology and how it is
+            transforming business.
+            <br/>I will be glad to work in a group with current technology stacks and high-level expertise.</p>
+          <p className={s.about__skill}>best skill on</p>
+          <ul className={s.about__items}>
+            {mySkills.map((el, i) =>
+              <motion.li key={i}
+                         whileHover={{scale:1.5}}
+                         style={{zIndex:100}}
+                         initial={{ opacity: 0, scale: 0.5 }}
+                         animate={{ opacity: 1, scale: 1 }}
+                         transition={{ duration: 1, repeatDelay:3, repeat:Infinity }}
+            >
+              <img
+                className={s.about__item}
+                src={el} alt=""/>
+            </motion.li>)}
+          </ul>
+
+        </div>
+        <div className={s.avatar}>
+
+          <div className={s.avatar__block}>
+            <img src={avatar} alt="avatar"/>
+          </div>
+
+        </div>
+
+      </div>
+      <Line/>
+    </section>
+  );
 };
 
 
