e7ef39cc668135260f3ecfbc00717ab777dfc2c7
Dmitriy Likhachev <likhachev.d.s@mad-soft.ru>
2024-11-27 09:01:10 +0400

    refactor

--- src/Resume/Me.tsx
+++ src/Resume/Me.tsx
@@ -4,7 +4,7 @@ import s from './Me.module.scss'
 const Me = () => {
   return (
     <div className={s.me}>
-      Hi! I'm Front-end Developer with experience about 2 years in creating SPA using React, Redux,
+      Hi! I'm Front-end Developer with experience about 3years in creating SPA using React, Redux,
       TypeScript and other technologies and libraries, with knowledge in user-interface,testing, and
       debugging processes. I know how to work in a team and achieve the best results. I improving my
       skills in this direction and plan to expand them with new technologies, such as Next JS. I
